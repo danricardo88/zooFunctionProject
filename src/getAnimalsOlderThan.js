654523a53f7b27cb88738737c14f2e03705d831a
danricardo88 <daniel.ricardo88@outlook.com>
2022-06-29 16:17:15 -0300

    requisito 2 - ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const buscaAnimal = species.find((animals) => animals.name === animal);
+  return buscaAnimal.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
