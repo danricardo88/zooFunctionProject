334c11f03d15ba3e7ab69fa9d765c7e57cafde8f
danricardo88 <daniel.ricardo88@outlook.com>
2022-06-29 19:26:47 -0300

    requisito 5 - ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const qnt = {};
+  if (!animal) {
+    species.forEach((pet) => {
+      qnt[pet.name] = pet.residents.length;
+    });
+    return qnt;
+  }
+  if (!animal.sex) {
+    return species.find((pet) => pet.name === animal.specie).residents.length;
+  }
+  return species.find((pet) => pet.name === animal.specie).residents
+    .filter((pet) => pet.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
