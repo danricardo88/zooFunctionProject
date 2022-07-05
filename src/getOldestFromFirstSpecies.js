90cfcee10c67acd1542b8e05131c1084179a5633
danricardo88 <daniel.ricardo88@outlook.com>
2022-07-05 00:25:37 -0300

    requisito 10 - ok - gloria a deus

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees, species } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const bicinho = employees
+    .find((ids) => ids.id === id).responsibleFor[0];
+  const ObjEspecie = species
+    .find((specie) => specie.id === bicinho).residents
+    .reduce((acc, item) => (item.age > acc.age ? item : acc));
+
+  return [ObjEspecie.name, ObjEspecie.sex, ObjEspecie.age];
+}
 module.exports = getOldestFromFirstSpecies;
